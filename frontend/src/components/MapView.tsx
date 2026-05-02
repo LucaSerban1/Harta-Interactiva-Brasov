1d85a3cb02e4bfd6d11d080967f46881d4c47535
dumy456 <dariusdumitrascu74@gmail.com>
2026-05-02 18:59:41 +0300

    feat: add MapView component with mock locations

---
+++ frontend/src/components/MapView.tsx
@@ -0,0 +1,41 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import type { Location } from '../mockData';
+import { mockLocations } from '../mockData';
+
+// fix icon-uri leaflet broken în Vite
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+});
+
+interface Props {
+  locations?: Location[];
+}
+
+export default function MapView({ locations = mockLocations }: Props) {
+  return (
+    <MapContainer
+      center={[45.6427, 25.5887]}
+      zoom={14}
+      style={{ width: '100%', height: '100vh' }}
+    >
+      <TileLayer
+        attribution='&copy; OpenStreetMap contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {locations.map((loc) => (
+        <Marker key={loc.id} position={[loc.lat, loc.lng]}>
+          <Popup>
+            <strong>{loc.name}</strong><br />
+            {loc.address}<br />
+            ⭐ {loc.rating}
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+}
